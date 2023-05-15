ef0ef195d2283891a7b4cd1008f568e5f4fd5848
Igor Floor <igor@mikha-el.com>
2023-05-15 02:48:58 -0300

    First commit

---
+++ src/main/kotlin/com/mikhael/kotlinkeyshelper/KotlinKeysHelperApplication.kt
@@ -0,0 +1,11 @@
+package com.mikhael.kotlinkeyshelper
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinKeysHelperApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinKeysHelperApplication>(*args)
+}
---
+++ src/test/kotlin/com/mikhael/kotlinkeyshelper/KotlinKeysHelperApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mikhael.kotlinkeyshelper
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinKeysHelperApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
